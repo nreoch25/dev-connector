d4e25c89f1ff7d6f61def876881608cf238564dd
Nigel Reoch <nreoch9@gmail.com>
2018-06-03 17:33:39 -0400

    Added express router

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
+// Api
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 // Initialize app
 const app = express();
 // DB Config
@@ -13,6 +17,11 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("hello world"));
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
