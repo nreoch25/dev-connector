dbb50b41cffe5aa2478c0e3a4eb2df3598519d21
Nigel Reoch <nreoch9@gmail.com>
2018-06-14 21:17:16 -0400

    Added jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/devconn"
+  mongoURI: "mongodb://localhost:27017/devconn",
+  secretOrKey: "secret"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+// Load user model
 const User = require("../../models/User");
+// Load config
+const keys = require("../../config/keys");
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -59,7 +63,17 @@ router.post("/login", (req, res) => {
     // Check password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "Success" });
+        // user matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+        // sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({ success: true, token: "Bearer " + token });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password incorrect" });
       }
