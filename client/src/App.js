897ceaf1d2c71baeb30df5bd75ea94a10bcf6e22
Nigel Reoch <nreoch9@gmail.com>
2018-07-07 12:36:13 -0400

    Added more to the profile section | Added delete account feature

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 import "./App.css";
 
 // Check for token
@@ -50,6 +51,13 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
